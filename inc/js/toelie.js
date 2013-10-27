ac253062f35c15e6914b2ba0ffa02bb993ea99d5
Kees Kluskens <keeskluskens@gmail.com>
2013-10-27 11:58:11 +0100

    Conflict fix

--- inc/js/toelie.js
+++ inc/js/toelie.js
@@ -4,46 +4,34 @@ $(function() {
 		bullets: false
 	});
 
-	$("#jplayer_sidebar").jPlayer({
-		ready: function() {
-			$(this).jPlayer("setMedia", {
-				m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-				oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-			});
-			$(this).jPlayer("setMedia", {
-				m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-				oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-			});
+	var myPlaylist = new jPlayerPlaylist({
+		jPlayer: "#jplayer_sidebar",
+		cssSelectorAncestor: "#jp_container_1"
+	}, [
+		{
+			title: "Jitterbug swing",
+			artist: "Ernest",
+			mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+			oga: "http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+		}
+	], {
+		playlistOptions: {
+			enableRemoveControls: true
 		},
 		swfPath: "/js",
-		supplied: "m4a, oga"
+		supplied: "ogv, m4v, oga, mp3",
+		smoothPlayBar: true,
+		keyEnabled: true,
 	});
-
-
-	//  Find any element starting with a # in the URL
-	//  And listen to any click events it fires
-	$('a[href^="#"]').click(function() {
-		//  Find the target element
-		var target = $($(this).attr('href'));
-
-		//  And get its position
-		var pos = target.offset(); // fallback to scrolling to top || {left: 0, top: 0};
-
-		//  jQuery will return false if there's no element
-		//  and your code will throw errors if it tries to do .offset().left;
-		if(pos) {
-			//  Scroll the page
-			$('html, body').animate({
-				scrollTop: pos.top,
-				scrollLeft: pos.left
-			}, 1000);
-		}
-
-		//  Don't let them visit the url, we'll scroll you there
-		return false;
-	});
-
-	var GoSquared = {acct: 'GSN-396664-U'};
-
 });
 
+		// ready: function() {
+		// 	$(this).jPlayer("setMedia", {
+		// 		m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+		// 		oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+		// 	});
+		// 	$(this).jPlayer("setMedia", {
+		// 		m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+		// 		oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+		// 	});
+		// },
