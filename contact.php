ed042fd9e6f92de2a39fa6d2a589b883719c7ce3
Jeroen <jeroen@MacBook-Pro-van-Jeroen.local>
2013-10-28 14:57:18 +0100

    Werkend contactformulier

--- contact.php
+++ contact.php
@@ -1,3 +1,7 @@
+<?php 
+session_start();
+?>
+
 <!DOCTYPE html>
 <!--[if IE 8]>               <html class="no-js lt-ie9" lang="nl" > <![endif]-->
 <!--[if gt IE 8]><!-->
@@ -54,7 +58,38 @@
                     <p><span style="color: #bc181f;"><strong>E-mailadres:</strong></span> ernest@checkdezewebsite.nl</p>
                     <p><span style="color: #bc181f;"><strong>Telefoonnummer:</strong></span> 0900-BOEKERNESTNU</p>
                     <p><img src="inc/img/layout/divider.png"></p>
-                    <?php
+                    <?php                    
+                    $mail_ontv = 'jeroen@laylo.nl';
+                    
+                    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+                    {
+                    	if (empty($_POST['naam']))
+                    		$naam_fout = 1;
+                    	if (function_exists('filter_var') && !filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL))
+                    		$email_fout = 1;
+                    	if (!empty($_SESSION['antiflood']))
+                    	{
+                    		$seconde = 20;
+                    		$tijd = time() - $_SESSION['antiflood'];
+                    		if($tijd < $seconde)
+                    		$antiflood = 1;
+                    	}
+                    }
+                    
+                    if (($_SERVER['REQUEST_METHOD'] == 'POST' && (!empty($antiflood) || empty($_POST['naam']) || !empty($naam_fout) || empty($_POST['mail']) || !empty($email_fout) || empty($_POST['bericht']) || empty($_POST['onderwerp']))) || $_SERVER['REQUEST_METHOD'] == 'GET')
+                    {
+                    	if ($_SERVER['REQUEST_METHOD'] == 'POST')
+                    	{
+                    		if (!empty($naam_fout))
+                    			echo '<p>Uw naam is niet ingevuld.</p>';
+                    		elseif (!empty($email_fout))
+                    			echo '<p>Uw e-mailadres is niet juist.</p>';
+                    		elseif (!empty($antiflood))
+                    			echo '<p>U mag slechts &eacute;&eacute;n bericht per ' . $seconde . ' seconde versturen.</p>';
+                    		else
+                    			echo '<p>U bent uw naam, e-mailadres, onderwerp of bericht vergeten in te vullen.</p>';
+                    	}
+                    	                    
                     echo '<form method="post" action="' . $_SERVER['REQUEST_URI'] . '" />
                       <p>
                           <label for="naam">Naam:</label>
@@ -76,6 +111,44 @@
                           <input type="submit" class="knopje" name="submit" value=" Versturen " />
                       </p>
                       </form>';
+                      
+                    }
+                    else
+                    {      
+                      $datum = date('d/m/Y H:i:s');
+                      $inhoud_mail = "===================================================\n";
+                      $inhoud_mail .= "Ingevuld contact formulier " . $_SERVER['HTTP_HOST'] . "\n";
+                      $inhoud_mail .= "===================================================\n\n";
+                      $inhoud_mail .= "Naam: " . htmlspecialchars($_POST['naam']) . "\n";
+                      $inhoud_mail .= "E-mail adres: " . htmlspecialchars($_POST['mail']) . "\n";
+                      $inhoud_mail .= "Bericht:\n";
+                      $inhoud_mail .= htmlspecialchars($_POST['bericht']) . "\n\n";
+                      $inhoud_mail .= "Verstuurd op " . $datum . " via het IP adres " . $_SERVER['REMOTE_ADDR'] . "\n\n";
+                      $inhoud_mail .= "Het ingevulde telefoonnummer: " . $telnummer . "\n\n";
+                      $inhoud_mail .= "===================================================\n\n";
+                      $headers = 'From: ' . htmlspecialchars($_POST['naam']) . ' <' . $_POST['mail'] . '>';
+                      $headers = stripslashes($headers);
+                      $headers = str_replace('\n', '', $headers);
+                      $headers = str_replace('\r', '', $headers);
+                      $headers = str_replace("\"", "\\\"", str_replace("\\", "\\\\", $headers));
+                      
+                      $_POST['onderwerp'] = str_replace('\n', '', $_POST['onderwerp']);
+                      $_POST['onderwerp'] = str_replace('\r', '', $_POST['onderwerp']);
+                      $_POST['onderwerp'] = str_replace("\"", "\\\"", str_replace("\\", "\\\\", $_POST['onderwerp']));
+                      
+                      if (mail($mail_ontv, $_POST['onderwerp'], $inhoud_mail, $headers))
+                      {
+                    	  $_SESSION['antiflood'] = time();	  
+                    	  echo '<h2>Het contactformulier is verzonden</h2>
+                    	  <p>Bedankt voor het invullen van het contactformulier. We zullen zo spoedig mogelijk contact met u opnemen.</p>';
+                      }
+                      else
+                      {
+                    	  echo '<h2>Het contactformulier is niet verzonden</h2>
+                    	  <p><b>Onze excuses.</b> Het contactformulier kon niet verzonden worden.</p>';
+                      }
+                    }
+                    
                     ?>
                 </div>
             </div>
